119b0f602b69feae43d13accbba654fd613a7c6f
amaanraz <amaanraz115@gmail.com>
2023-05-30 18:45:44 -0700

    Add files via upload

---
+++ disasm.c
@@ -0,0 +1,236 @@
+#include <stdio.h> // for stderr
+#include <stdlib.h> // for exit()
+#include "types.h"
+#include "utils.h"
+
+void print_rtype(char *, Instruction);
+void print_itype_except_load(char *, Instruction, int);
+void print_load(char *, Instruction);
+void print_store(char *, Instruction);
+void print_branch(char *, Instruction);
+void print_lui(Instruction);
+void print_jal(Instruction);
+void print_ecall(Instruction);
+void write_rtype(Instruction);
+void write_itype_except_load(Instruction); 
+void write_load(Instruction);
+void write_store(Instruction);
+void write_branch(Instruction);
+
+
+void decode_instruction(uint32_t instruction_bits) {
+    Instruction instruction = parse_instruction(instruction_bits);
+    switch(instruction.opcode) {
+        case 0x33:
+            write_rtype(instruction);
+            break;
+        case 0x13:
+            write_itype_except_load(instruction);
+            break;
+        case 0x3:
+            write_load(instruction);
+            break;
+        case 0x23:
+            write_store(instruction);
+            break;
+        case 0x63:
+            write_branch(instruction);
+            break;
+        case 0x37:
+            print_lui(instruction);
+            break;
+        case 0x6F:
+            print_jal(instruction);
+            break;
+        case 0x73:
+            print_ecall(instruction);
+            break;
+        default: // undefined opcode
+            handle_invalid_instruction(instruction);
+            break;
+    }
+}
+
+void write_rtype(Instruction instruction) {
+    /* YOUR CODE HERE */
+    switch (instruction.rtype.funct3) {
+        case 0x0:
+            switch (instruction.rtype.funct7) {
+                case 0x0:
+		            print_rtype("add", instruction);
+                    break;
+		        case 0x1:
+                    print_rtype("mul", instruction);
+                    break;
+                case 0x20:
+                    print_rtype("sub", instruction);
+                    break;
+                default:
+                    handle_invalid_instruction(instruction);
+                break;      
+            }
+            break;
+        case 0x01:
+            switch(instruction.rtype.funct7){
+                case 0x0:
+                    print_rtype("sll", instruction);
+                    break;
+                case 0x01:
+                    print_rtype("mulh", instruction);
+                    break;
+                default:
+                    handle_invalid_instruction(instruction);
+                break; 
+            }
+            break;
+        case 0x2:
+            switch (instruction.rtype.funct7){
+                case 0x0:
+                    print_rtype("slt", instruction);
+                    break;
+                default:
+                    handle_invalid_instruction(instruction);
+                break; 
+            }
+            break;
+        case 0x4:
+            switch(instruction.rtype.funct7){
+                case 0x0:
+                    print_rtype("xor", instruction);
+                    break;
+                case 0x01:
+                    print_rtype("div", instruction);
+                    break;
+                default:
+                    handle_invalid_instruction(instruction);
+                break;
+            }
+            break;
+        case 0x5:
+            switch(instruction.rtype.funct7){
+                case 0x0:
+                    print_rtype("srl", instruction);
+                    break;
+                case 0x20:
+                    print_rtype("sra", instruction);
+                    break;
+                default:
+                    handle_invalid_instruction(instruction);
+                break;
+            }
+            break;
+        case 0x6:
+            switch(instruction.rtype.funct7){
+                case 0x0:
+                    print_rtype("or", instruction);
+                    break;
+                case 0x01:
+                    print_rtype("rem", instruction);
+                    break;
+                default:
+                    handle_invalid_instruction(instruction);
+                break;
+            }
+            break;
+        case 0x7:
+            switch(instruction.rtype.funct7){
+                case 0x0:
+                    print_rtype("and", instruction);
+                    break;
+                default:
+                    handle_invalid_instruction(instruction);
+                break;
+            }
+            break;
+        /* call print_rtype */
+        default:
+            handle_invalid_instruction(instruction);
+        break;
+    }
+}
+
+void write_itype_except_load(Instruction instruction) {
+    switch (instruction.itype.funct3) {
+      /* YOUR CODE HERE */
+      /* call print_itype_except_load */
+        default:
+            handle_invalid_instruction(instruction);
+            break;  
+    }
+}
+
+void write_load(Instruction instruction) {
+    switch (instruction.itype.funct3) {
+        case 0x0:
+            print_load("lb", instruction);
+            break;
+        case 0x1:
+            print_load("lh", instruction);
+            break;
+        case 0x2:
+            print_load("lw", instruction);
+            break;
+      /* call print_load */
+        default:
+            handle_invalid_instruction(instruction);
+            break;
+    }
+}
+
+void write_store(Instruction instruction) {
+    switch (instruction.stype.funct3) {
+      /* YOUR CODE HERE */
+      /* call print_store */
+        default:
+            handle_invalid_instruction(instruction);
+            break;
+    }
+}
+
+void write_branch(Instruction instruction) {
+    switch (instruction.sbtype.funct3) {
+        case 0x0:
+            print_branch("beq", instruction);
+            break;
+        case 0x1:
+            print_branch("bne", instruction);
+            break;
+      /* call print_branch */
+        default:
+            handle_invalid_instruction(instruction);
+        break;
+    }
+}
+
+void print_rtype(char *name, Instruction instruction) {
+  printf(RTYPE_FORMAT, name, instruction.rtype.rd, instruction.rtype.rs1,
+         instruction.rtype.rs2);
+}
+
+void print_itype_except_load(char *name, Instruction instruction, int imm) {
+    /* YOUR CODE HERE */
+}
+
+void print_load(char *name, Instruction instruction) {
+    printf(MEM_FORMAT, name, instruction.itype.rd, instruction.itype.imm, instruction.itype.rs1);
+}
+
+void print_store(char *name, Instruction instruction) {
+    /* YOUR CODE HERE */
+}
+
+void print_branch(char *name, Instruction instruction) {
+    printf(BRANCH_FORMAT, name, instruction.sbtype.rs1, instruction.sbtype.rs2, get_branch_offset(instruction));
+}
+
+void print_lui(Instruction instruction) {
+    /* YOUR CODE HERE */
+}
+
+void print_jal(Instruction instruction) {
+    printf(JAL_FORMAT, instruction.ujtype.rd, get_jump_offset(instruction));
+}
+
+void print_ecall(Instruction instruction) {
+    printf(ECALL_FORMAT);
+}
---
+++ utils.c
@@ -0,0 +1,196 @@
+#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Unpacks the 32-bit machine code instruction given into the correct
+ * type within the instruction struct */
+Instruction parse_instruction(uint32_t instruction_bits) {
+  /* YOUR CODE HERE */
+  Instruction instruction;
+  // add x9, x20, x21   hex: 01 5A 04 B3, binary = 0000 0001 0101 1010 0000 0100 1011 0011
+  // Opcode: 0110011 (0x33) Get the Opcode by &ing 0x1111111, bottom 7 bits
+  instruction.opcode = instruction_bits & ((1U << 7) - 1);
+
+  // Shift right to move to pointer to interpret next fields in instruction.
+  instruction_bits >>= 7;
+
+  switch (instruction.opcode) {
+  // R-Type
+  case 0x33:
+    // instruction: 0000 0001 0101 1010 0000 0100 1, destination : 01001
+    instruction.rtype.rd = instruction_bits & ((1U << 5) - 1);
+    instruction_bits >>= 5;
+
+    // instruction: 0000 0001 0101 1010 0000, func3 : 000
+    instruction.rtype.funct3 = instruction_bits & ((1U << 3) - 1);
+    instruction_bits >>= 3;
+
+    // instruction: 0000 0001 0101 1010 0, src1: 10100
+    instruction.rtype.rs1 = instruction_bits & ((1U << 5) - 1);
+    instruction_bits >>= 5;
+
+    // instruction: 0000 0001 0101, src2: 10101
+    instruction.rtype.rs2 = instruction_bits & ((1U << 5) - 1);
+    instruction_bits >>= 5;
+
+    // funct7: 0000 000
+    instruction.rtype.funct7 = instruction_bits & ((1U << 7) - 1);
+    break;
+  
+  // LOAD
+  case 0x3:
+
+    instruction.itype.rd = instruction_bits & ((1U << 5) - 1);
+    instruction_bits >>= 5;
+
+    instruction.itype.funct3 = instruction_bits & ((1U << 3) - 1);
+    instruction_bits >>= 3;
+
+    instruction.itype.rs1 = instruction_bits & ((1U << 5) - 1);
+    instruction_bits >>= 5;
+
+    instruction.itype.imm = instruction_bits & ((1U << 12) - 1);
+    break;
+  //SBtype
+  case 0x63:
+    // opcode, imm5, funct3, rs1, rs2, imm7
+
+    instruction.sbtype.imm5 = instruction_bits & ((1U << 5) - 1);
+    instruction_bits >>= 5;
+
+    instruction.sbtype.funct3 = instruction_bits & ((1U << 3) - 1);
+    instruction_bits >>= 3;
+
+    instruction.sbtype.rs1 = instruction_bits & ((1U << 5) - 1);
+    instruction_bits >>= 5;
+
+    instruction.sbtype.rs2 = instruction_bits & ((1U << 5) - 1);
+    instruction_bits >>= 5;
+
+    instruction.sbtype.imm7 = instruction_bits & ((1U << 7) - 1);
+    break;
+
+  // UJ type
+  case 0x6f:
+
+    // rd
+    instruction.ujtype.rd = instruction_bits & ((1U << 5) - 1);
+    instruction_bits >>= 5;
+
+    instruction.ujtype.imm = instruction_bits & ((1U << 20) - 1);
+    break;
+  
+  // ECALL
+  case 0x73:
+    instruction.opcode = 0x73;
+    break;
+  
+  // cases for other types of instructions
+  /* YOUR CODE HERE */
+
+  #ifndef TESTING
+  default:
+    exit(EXIT_FAILURE);
+  #endif
+  }
+  return instruction;
+}
+
+/************************Helper functions************************/
+/* Here, you will need to implement a few common helper functions, 
+ * which you will call in other functions when parsing, printing, 
+ * or executing the instructions. */
+
+/* Sign extends the given field to a 32-bit integer where field is
+ * interpreted an n-bit integer. */
+int sign_extend_number(unsigned int field, unsigned int n) {
+  /* YOUR CODE HERE */
+  return 0;
+}
+
+/* Return the number of bytes (from the current PC) to the branch label using
+ * the given branch instruction */
+int get_branch_offset(Instruction instruction) {
+  // imm[12|10:5] imm[4:1|11] 0 bit: 0
+  // 1111 1110 1100 1101 1000 0000 1110 0011
+
+  // opcode : 110 0011 -> 0x63
+  // imm5 : 00001
+  // imm7: 1111111
+
+  // ex: offset = 1 1 11111 0000 0 -> signed : -32
+
+  unsigned int imm5 = instruction.sbtype.imm5;
+  unsigned int imm7 = instruction.sbtype.imm7;
+
+  // 1:4 00000
+  int var1 = imm5 & 0x1E;
+
+  // 5:10 00011111
+  int var2 = imm7 & 0x1F;
+
+  // 11 00001
+  int var3 = imm5 & 0x1;
+
+  // 12 01000000
+  int var4 = imm7 & 0x40;
+  int offset = (var4 << 5) | (var3 << 10) | (var2 << 5) | var1;
+  // check msb for sign
+  int sign_bit = offset >> 11;
+  if(sign_bit){
+    int all = 0xFFF;
+    // return signed representation
+    offset = offset - all - 1;
+  }
+  return offset;
+}
+
+/* Returns the number of bytes (from the current PC) to the jump label using the
+ * given jump instruction */
+int get_jump_offset(Instruction instruction) {
+  // restucture the imm value; imm[20|10:1|11|19:12] 20 bits long
+
+  int imm = instruction.ujtype.imm;
+  // 19:12
+  int var1 = imm & 0xFF;
+  // 11
+  int var2 = (imm >> 8) & 0x1;
+  //10:1 
+  int var3 = ((imm >> 9) & 0x3FF) << 1;
+  // 20
+  int var4 = (imm >> 19) & 0x1;
+
+  int offset = (var4 << 19) | (var1 << 11) | (var2 << 10) | var3;
+
+  // check msb for sign
+  int sign_bit = offset >> 19;
+  if(sign_bit){
+    int all = 0xFFFFF;
+    // return signed representation
+    offset = offset - all - 1;
+  }
+
+  return offset;
+}
+
+/* Returns the number of bytes (from the current PC) to the base address using the
+ * given store instruction */
+int get_store_offset(Instruction instruction) {
+  /* YOUR CODE HERE */
+  return 0;
+}
+/************************Helper functions************************/
+
+void handle_invalid_instruction(Instruction instruction) {
+  printf("Invalid Instruction: 0x%08x\n", instruction.bits);
+}
+
+void handle_invalid_read(Address address) {
+  printf("Bad Read. Address: 0x%08x\n", address);
+  exit(-1);
+}
+
+void handle_invalid_write(Address address) {
+  printf("Bad Write. Address: 0x%08x\n", address);
+  exit(-1);
+}
